3741e8d67ff18a91063c817cbe6b3f19156af187
JunoJulius <yunoyulius@gmail.com>
2018-06-10 19:10:31 +0200

    all green after refactoring async

--- src/ShoppingCart.js
+++ src/ShoppingCart.js
@@ -1,50 +1,38 @@
 class ShoppingCart {
 
   constructor(items) {
-
     this.items = []
-
   }
 
-  addItem(name, quantity, pricePerUnit) {
-    const shopCart = this.items.push({
-      name: name,
-      quantity: quantity,
-      pricePerUnit: pricePerUnit
-    })
+      addItem(name, quantity, pricePerUnit) {
+        const shopCart = this.items.push({
+          name: name,
+          quantity: quantity,
+          pricePerUnit: pricePerUnit
+        })
 
-    return shopCart
-  }
+        return shopCart
+      }
 
-  getItems(...items) {
-    const displayItems = this.items
-    return displayItems
+      getItems(...items) {
+        const displayItems = this.items
+        return displayItems
 
-  }
+      }
 
-  clear(...items) {
-    const emptyCart = this.items.length = []
-    return emptyCart
-  }
+      clear(...items) {
+        const emptyCart = this.items.length = []
+        return emptyCart
+      }
 
-  clone(...items) {
-    const copyCart = new ShoppingCart()
-    copyCart.items = JSON.parse(JSON.stringify(this.items))
-    return copyCart
+      clone(...items) {
+        const copyCart = new ShoppingCart()
+        copyCart.items = JSON.parse(JSON.stringify(this.items))
+        return copyCart
 
-  }
+      }
 
 
 }
-
-
-//
-// const cart1 = new ShoppingCart('banana', 12, 23)
-// const cart2 = cart1.clone()
-// //
-// console.log(cart2)
-// //
-
-
 
 module.exports = ShoppingCart;
--- src/async-functions.js
+++ src/async-functions.js
@@ -1,51 +1,29 @@
-function giveItBackLater(value, callback) {
-  const giveItBack = () => {
-    callback(value)
-  }
-  setTimeout(giveItBack, 1000, "Hellooo World")
-}
-
-function promiseToGiveItBackLater(value) {
-
-  return new Promise((resolve, reject) => {
-    const giveItBack = () => {
-      if (Math.random(value) < 0.5 || Math.random(value > 0.5)) {
-        resolve(value)
-      } else {
-        reject('Erroooarrr!')
+const giveItBackLater = (value, callback) => {
+      const giveItBack = () => {
+        callback(value)
       }
-    }
-    setTimeout(giveItBack, 100)
-  })
+      setTimeout(giveItBack, 1000, "Hellooo World")
 }
 
-function addSomePromises(somePromise) {
-  // 
-  // return new Promise(function(resolve, reject) {
-  //
-  //   if (somePromise === true) {
-  //
-  //     resolve(somePromise)
-  //
-  //   } else {
-  //
-  //     reject()
-  //   }
-  //
-  // })
+const promiseToGiveItBackLater = (value) => {
 
+    return new Promise((resolve, reject) => {
+      const giveItBack = () => {
+        if (Math.random(value) < 0.5 || Math.random(value > 0.5)) {
+          resolve(value)
+        } else {
+          reject('Erroooarrr!')
+        }
+      }
+      setTimeout(giveItBack, 100)
+    })
 }
 
-
-//
-// When the code `const outputPromise = addSomePromises(somePromise)` is executed,
-//  your function should return a new promise that has both a fulfillment handler and a rejection handler.
-//
-//      - When `somePromise` resolves with a string `"foo"`, the `outputPromise` should resolve with a string `"foofoo"`.
-//      - When `somePromise` is rejected with the value `"bar"`, the `outputPromise`  should resolve with `"barbarbar"`.
-//
-//    So, your fulfillment handler should double the string and the rejection handler should triple the string.
-//
+ const addSomePromises = (somePromise) => {
+    return new Promise((resolve, reject) => resolve(somePromise))
+      .then((result) => result.repeat(2))
+      .catch((error) => error.repeat(3))
+  }
 
 
 module.exports = {
